0ac756bb0ed0a3648d9ce08538b5ad7ccf98a9ad
Hui Chen <hchen5796@gmail.com>
2023-06-21 17:41:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,6 +9,14 @@ def read_root():
     return {"Hello": "World"}
 
 
+# Allow cross domain requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
